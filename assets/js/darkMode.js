f42247ae1ebd600d0fa7204573db958237066854
Maaz-Code <500077097@stu.upes.ac.in>
2022-01-29 12:37:36 +0530

    Updated the site and created future space for darkmode

---
+++ assets/js/darkMode.js
@@ -0,0 +1,15 @@
+//darkmode functionality
+//  const options = {
+//     top: '32px',
+//     left: '32px', // default: 'unset'
+//     time: '0.5s', // default: '0.3s'
+//     mixColor: '#fff', // default: '#fff'
+//       backgroundColor: '#181818',  // default: '#fff'
+//       buttonColorDark: '#181818',  // default: '#100f2c'
+//       buttonColorLight: '#fff', // default: '#fff'
+//       saveInCookies: false, // default: true,
+//       label: '🌓', // default: ''
+//       autoMatchOsTheme: false // default: true
+//     }
+//     const darkmode = new Darkmode(options);
+//     darkmode.showWidget();
--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@
       backDelay: 2000
     });
   }
-
+    
   // Smooth scroll for the navigation menu and links with .scrollto classes
   $(document).on('click', '.nav-menu a, .scrollto', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
